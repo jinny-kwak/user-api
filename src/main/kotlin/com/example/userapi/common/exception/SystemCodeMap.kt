3017be4711e9fb9fb465d476674888f30e6ad302
jinny <jinny@sentbe.com>
2025-06-16 22:54:34 +0900

    :recycle: enum message 수정

--- src/main/kotlin/com/example/userapi/common/exception/SystemCodeMap.kt
+++ src/main/kotlin/com/example/userapi/common/exception/SystemCodeMap.kt
@@ -4,8 +4,8 @@ enum class SystemCodeMap(
     private val systemCode : String,
     private val systemMessage : String
 ) {
-    STATUS_SUCCESS("SUCCESS", "요청이 성공했습니다."),
-    STATUS_FAIL("FAIL", "요청이 실패했습니다.")
+    STATUS_SUCCESS("SUCCESS", "요청을 성공했습니다."),
+    STATUS_FAIL("FAIL", "요청을 실패했습니다.")
     ;
 
     fun getSystemCode(): String {
